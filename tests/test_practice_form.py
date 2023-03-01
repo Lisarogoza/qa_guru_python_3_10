76d3a841209e60bc5f76a3c3e51aa9e5ae28f602
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2023-03-01 15:29:04 +0300

    Added attach file

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,6 +1,9 @@
 import allure
+from selene import browser
+
 from demoqa_project.model.data.users_data import user
 from demoqa_project.model.pages.practice_form import TestFormPage
+from tests.utils import attach
 
 
 @allure.title("Fill form successful")
@@ -11,3 +14,6 @@ def test_practice():
     with allure.step("Assert results"):
         user_olga.assert_fields()
 
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
