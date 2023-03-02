37c5439804de5c33ab2e18047979969323905554
Ксения <118900137+Lisarogoza@users.noreply.github.com>
2023-03-02 10:48:55 +0300

    Added collect video option to test file

--- tests/test_practice_form.py
+++ tests/test_practice_form.py
@@ -1,6 +1,7 @@
 import allure
-from selene import browser
-
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from demoqa_project.model.data.users_data import user
 from demoqa_project.model.pages.practice_form import TestFormPage
 from tests.utils import attach
@@ -8,6 +9,23 @@ from tests.utils import attach
 
 @allure.title("Fill form successful")
 def test_practice():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+    browser.config.driver = driver
+
     user_olga = TestFormPage(user)
     with allure.step("Fill form"):
         user_olga.fill_form().submit()
@@ -17,3 +35,4 @@ def test_practice():
     attach.add_html(browser)
     attach.add_screenshot(browser)
     attach.add_logs(browser)
+    attach.add_video(browser)
